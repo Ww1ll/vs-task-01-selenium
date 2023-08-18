0c4fc4a22a74fa456b7d6fda76909e829bfb4ea8
Kassia do Espirito Santo <kassiaes@gmail.com>
2023-08-17 23:04:34 -0300

    test case 11

---
+++ tafe_project/src/main/java/Tests/TestCase11.java
@@ -0,0 +1,70 @@
+/**
+ * 1. Launch browser
+ * 2. Navigate to url 'http://automationexercise.com'
+ * 3. Verify that home page is visible successfully
+ * 4. Click 'Cart' button
+ * 5. Scroll down to footer
+ * 6. Verify text 'SUBSCRIPTION'
+ * 7. Enter email address in input and click arrow button
+ * 8. Verify success message 'You have been successfully subscribed!' is visible
+ */
+
+package Tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestCase11 {
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    @BeforeTest
+    public void abrirNavegador() {
+        String caminhoDriver= "driver/chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", caminhoDriver);
+        driver= new ChromeDriver();
+        wait = new WebDriverWait(driver, 40);
+        driver.get("https://www.automationexercise.com/");
+        driver.manage().window().maximize();
+    }
+
+    @AfterTest
+    public void finalizarNavegador() {
+        driver.quit();
+    }
+
+    @Test
+    public void testMustSearchProductSuccessfully() {
+
+        String cartButton = "#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(3) > a";
+        String subscription = "#footer > div.footer-widget > div > div > div.col-sm-3.col-sm-offset-1 > div > h2";
+        String inputButton = "#susbscribe_email";
+        String subscribeButton = "#subscribe";
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(cartButton)));
+        driver.findElement(By.cssSelector(cartButton)).click();
+
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(By.cssSelector(subscription))).perform();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(inputButton)));
+        driver.findElement(By.cssSelector(inputButton)).sendKeys("qa@gmail.com");
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(subscribeButton)));
+        driver.findElement(By.cssSelector(subscribeButton)).click();
+
+
+        // esta mensagem aparece por poucos segundos, não consigo pegar o seletor...
+        // 8. Verify success message 'You have been successfully subscribed!' is visible
+
+    }
+
+
+}

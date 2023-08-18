7b65cbe794be444c893b0e5fdca2ab1ad1830d39
Kassia do Espirito Santo <kassiaes@gmail.com>
2023-08-18 13:14:09 -0300

    test case 10

---
+++ tafe_project/src/main/java/Tests/TestCase10.java
@@ -0,0 +1,57 @@
+package Tests;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestCase10 {
+
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    @BeforeTest
+    public void abrirNavegador() {
+        String caminhoDriver= "driver/chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", caminhoDriver);
+        driver= new ChromeDriver();
+        wait = new WebDriverWait(driver, 40);
+        driver.get("https://www.automationexercise.com/");
+        driver.manage().window().maximize();
+    }
+
+    @AfterTest
+    public void finalizarNavegador() {
+        driver.quit();
+    }
+
+    @Test
+    public void testSubscriptionInHomePageSuccessfully() {
+
+        Faker faker = new Faker();
+
+        String subscription = "#footer > div.footer-widget > div > div > div.col-sm-3.col-sm-offset-1 > div > h2";
+        String inputButton = "#susbscribe_email";
+        String subscribeButton = "#subscribe";
+
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(By.cssSelector(subscription))).perform();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(inputButton)));
+        driver.findElement(By.cssSelector(inputButton)).sendKeys(faker.internet().emailAddress());
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(subscribeButton)));
+        driver.findElement(By.cssSelector(subscribeButton)).click();
+
+        Assert.assertEquals("You have been successfully subscribed!", driver.findElement(By.cssSelector("#success-subscribe > div")).getText());
+
+    }
+
+}
